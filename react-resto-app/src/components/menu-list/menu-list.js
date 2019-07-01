829592975a2812eed31cf844fd046272d4d14e5e
Евгений Дружняев <druzhniaev@gmail.com>
2019-07-01 23:42:39 +0600

    15 урок правка

--- react-resto-app/src/components/menu-list/menu-list.js
+++ react-resto-app/src/components/menu-list/menu-list.js
@@ -43,7 +43,8 @@ class MenuList extends Component {
 const mapStateToProps = (state) => {
     return {
         menuItems: state.menu,
-        loading: state.loading
+        loading: state.loading,
+        error: state.error
     }
 };
 
